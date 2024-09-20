724943639afb841b23d6d7fa953697b4ada2632a
Anastasiia <shalygina.aa@azs-opti.local>
2024-09-20 15:01:43 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/DuplicatedDataException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class DuplicatedDataException extends RuntimeException {
-    public DuplicatedDataException(String message) {
-        super(message);
-    }
-}
